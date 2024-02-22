f8fb0c7b778025fd042437afec4ca4220cafe1ee
VladKn1 <70967859+VladKn1@users.noreply.github.com>
2024-02-22 19:15:25 +0300

    backJava_site

---
+++ src/main/java/com/example/buysell/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.buysell.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.buysell.models.User;
+public interface UserRepository extends JpaRepository<User,Long > {
+User findByEmail(String email);
+}
